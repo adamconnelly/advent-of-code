70149d1705e343b968bbc65c10d9399231fe09ce
Adam Connelly <adam.rpconnelly@gmail.com>
2017-12-11 21:19:33 +0000

    Adding solution to level 1, part 2

--- 2017/level1/captcha.js
+++ 2017/level1/captcha.js
@@ -2,16 +2,19 @@
  * Returns the answer to part 1 of the advent of code challenge.
  * 
  * @param {String} input - the input to the captcha
+ * @param {Number} lookahead - indicates how many elements to lookahead in the list when finding matching digits. Defaults to 1.
  */
-module.exports = (input) => {
+module.exports = (input, lookahead) => {
     if (!input) {
         return 0
     }
 
+    lookahead = lookahead || 1
+
     let sum = 0
 
     for (let i = 0; i < input.length; i++) {
-        const nextIndex = i === input.length - 1 ? 0 : i + 1
+        const nextIndex = (i + lookahead) % input.length
         if (input[i] == input[nextIndex]) {
             sum += parseInt(input[i])
         }
